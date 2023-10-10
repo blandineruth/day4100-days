781779b231d1f24253c2b31a95a64d305fd527f7
blandineruth <104473080+blandineruth@users.noreply.github.com>
2023-10-10 04:45:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+
+print("Welcome to your adventure simulator")
+print()
+
+# Question
+
+nameFamilly = input("what is your name familly ?")
+print()
+nameEnemy = input("what is your worst enemy's name ?")
+print()
+superPower = input("What is your superpower?")
+print()
+fav = input("What is your favorite food?")
+print()
+liveyou = input("where do you live ?")
+print()
+favjob = input("what would you like make your life ?")
+print()
+historylife = input("what will you be doing in 3 years ?")
+print()
+
+
+print(" Hello Everybody, today i will be speaking english to explicate my history are you ready? my name is",  nameFamilly,"my worst enem'y is ", nameEnemy, "my superpower is", superPower, "and my favorite food is", fav, "i live in",liveyou, "and i want to be a", favjob, "i will be doing", historylife, "in 3 years olds" ) 
+
+
+
+
+
+
